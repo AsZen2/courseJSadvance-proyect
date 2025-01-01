a626b151f1fe9b16a458514a816b88d3f48b335a
AsZen2 <nahuel.matozo@gmail.com>
2025-01-01 17:26:58 -0300

    improving responsive desing

--- functions.js
+++ functions.js
@@ -1,5 +1,30 @@
+
+
+
+// mobile menu
+
+let icon_menu = document.getElementById('icon_menu')
+let icon_menu2 = document.getElementById('icon_menu2')
+let mobile_menu = document.getElementById('content-menu-mobile')
+
+
+// detecting scrollY
+
 window.addEventListener('scroll', function (){
     let nav = this.document.querySelector(('nav'))
     nav.classList.toggle('down', this.window.scrollY > 300);
-
 })
+
+
+icon_menu.addEventListener('click', () => {
+    mobile_menu.style.display = 'flex'
+    icon_menu.style.display = 'none'
+})
+
+icon_menu2.addEventListener('click', () => {
+    mobile_menu.style.display = 'none'
+    icon_menu.style.display = 'flex'
+})
+
+
+
